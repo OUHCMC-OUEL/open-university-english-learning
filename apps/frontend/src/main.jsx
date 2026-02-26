cb2212803f53a8a13550aed3054ebcca117d3b0a
JuniorThanh <2351050164thanh@ou.edu.vn>
2026-02-26 19:15:51 +0700

    function_add(oauth, user_profile, UI/UX, app_info): Major update for ouel app
    v3.3.8

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './configs/AuthContext.js'
 import { ToastProvider } from '@/components/Context/toastContext.jsx';
-import { AlertDialogProvider } from '@/components/Context/AlertDialogContext';
+import { AlertDialogProvider } from '@/components/Context/alertDialogContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
